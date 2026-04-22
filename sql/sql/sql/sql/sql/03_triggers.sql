14423c7adf71f1c83a4b425040e4d78bfcf65a22
Gayatri Ravindra  Shirsath <113703193+Gayatri242003@users.noreply.github.com>
2026-04-22 19:52:49 +0530

    Delete sql/sql/sql/sql/sql directory

--- sql/sql/sql/sql/sql/03_triggers.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TRIGGER emp_insert_trigger
-AFTER INSERT ON employees
-FOR EACH ROW
-BEGIN
-  INSERT INTO emp_audit (action_type, emp_id, action_date)
-  VALUES ('INSERT', :NEW.emp_id, SYSDATE);
-END;
-/
